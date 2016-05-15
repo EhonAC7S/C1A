417ab92eee9b0a8632cff4a17912fe69b5da1351
EhonAC7S <jordan.cesaro@telecomnancy.net>
2016-05-15 21:02:39 +0200

    ajout du sous programme Depense pour creer le relevé de compte

---
+++ budget/src/entreTransaction.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	FILE* fichier = NULL;	
+	char date[10] = "";
+	char moyen[7] = "";
+	char dest[20] = "";
+	double prix;
+	char cat[20] = "";
+	char cont='y';
+	fichier = fopen("transaction.info","a+");
+	printf("Veuillez suivre les instructions suivantes : ");
+	do {
+		printf("Entrez une date de la forme : jj/mm/aaaa : ");
+		scanf("%s",date);
+		printf("Entrez le moyen de transaction parmis 'CB','Liquide' et 'Chèque' : ");
+		scanf("%s",moyen);
+		printf("Entrez le destinataire (Max 20 caractères sans espace) : ");
+		scanf("%s",dest);
+		printf("Entrez la somme dépensée : ");
+		scanf("%lf",&prix);
+		printf("Entrez la catégorie d'achat parmi Immobilier, Alimentaire, Imprévu, Essence, Extra : ");
+		scanf("%s",cat);
+		fprintf(fichier,"%s,%s,%s,%.2f \n",date,moyen,dest,prix);
+		printf("Continuer? (y/n) : ");
+		scanf("%s",&cont);
+
+	} while (cont=='y');
+	
+
+return 0;
+}
